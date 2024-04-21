7dae68d8b307a0ca62cb40862933fb19a7595b2c
Roba1bk <166316036+Roba1bk@users.noreply.github.com>
2024-04-21 15:31:34 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let navb = document.querySelector('.header');
+
+            window.onscroll = function()
+            {
+                if(window.scrollY > 0){
+                    navb.style.background = '#6b2026';
+                }
+            }
